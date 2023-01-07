97187505e560c52fe16715c6ff3f12805a348b7f
phurikornt <mysteriousphu@gmail.com>
2023-01-07 15:50:47 +0700

    Add All Route for User

--- shabu-develop/routes/user.js
+++ shabu-develop/routes/user.js
@@ -4,8 +4,19 @@ const express = require('express');
 
 const user = require('../controllers/user')
 
+const conSet_materials = require('../controllers/module/set_materials')
+const conSale = require('../controllers/module/sale')
+
 const router = express.Router();
 
 router.get('/', user.getEmployee);
+
+
+// ====================== Route รับวัตถุดิบเข้า======================
+router.get('/set_materials', conSet_materials.getSet_materials);
+
+// ====================== Route การขาย ======================
+router.get('/sale', conSale.getSale);
+
 
 module.exports = router;
